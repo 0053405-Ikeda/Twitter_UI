0f4b33846c175ea3f2b5d1996503ad2d6dedb5f4
Ikeda <128609683+0053405-Ikeda@users.noreply.github.com>
2023-04-01 18:01:34 +0900

    お気に入りアイコンを追加

--- mytwitter/lib/main.dart
+++ mytwitter/lib/main.dart
@@ -43,7 +43,11 @@ class MyApp extends StatelessWidget {
                     ],
                   ),
                   SizedBox(height: 4),
-                  Text('最高でした')
+                  Text('最高でした'),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.favorite_border),
+                  )
                 ],
               ),
             ],
