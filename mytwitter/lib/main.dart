33d0b9562edd3e01815258b1ba0652dd533e4d20
Ikeda <128609683+0053405-Ikeda@users.noreply.github.com>
2023-04-01 16:59:02 +0900

    アイコン画像を追加

--- mytwitter/lib/main.dart
+++ mytwitter/lib/main.dart
@@ -24,18 +24,28 @@ class MyApp extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://pbs.twimg.com/profile_images/1626936992729821184/JpNi95LG_400x400.jpg'),
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('T.Ikeda@TIkeda93820944'),
-                  SizedBox(width: 8),
-                  Text('2023/04/01'),
+                  Row(
+                    children: [
+                      Text('T.Ikeda@TIkeda93820944'),
+                      SizedBox(width: 8),
+                      Text('2023/04/01'),
+                    ],
+                  ),
+                  SizedBox(height: 4),
+                  Text('最高でした')
                 ],
               ),
-              SizedBox(height: 4),
-              Text('最高でした')
             ],
           ),
         ),
