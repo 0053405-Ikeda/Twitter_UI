6bf9ce9076aa3a2a5b014bff5f704756fd9841ec
Ikeda <128609683+0053405-Ikeda@users.noreply.github.com>
2023-04-01 21:50:23 +0900

    TLのコメントを複数表示

--- mytwitter/lib/main.dart
+++ mytwitter/lib/main.dart
@@ -15,44 +15,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'T.Ikeda@TIkeda93820944',
+            'T.Ikeda',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 16,
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        body: SingleChildScrollView(
+          child: Column(
             children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://pbs.twimg.com/profile_images/1626936992729821184/JpNi95LG_400x400.jpg'),
-              ),
-              SizedBox(width: 8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text('T.Ikeda@TIkeda93820944'),
-                      SizedBox(width: 8),
-                      Text('2023/04/01'),
-                    ],
-                  ),
-                  SizedBox(height: 4),
-                  Text('最高でした'),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.favorite_border),
-                  )
-                ],
-              ),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://pbs.twimg.com/profile_images/1626936992729821184/JpNi95LG_400x400.jpg'),
+          ),
+          SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text('T.Ikeda@TIkeda93820944'),
+                  SizedBox(width: 8),
+                  Text('2023/04/01'),
+                ],
+              ),
+              SizedBox(height: 4),
+              Text('最高でした'),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
